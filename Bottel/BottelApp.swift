f57f425126208e7b4f5cdfec49e3fbcc6f82eb35
danadajian <danadajian@gmail.com>
2022-02-03 18:55:19 -0600

    Initial Commit

---
+++ Bottel/BottelApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottelApp.swift
+//  Bottel
+//
+//  Created by Dan Adajian on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BottelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bottel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bottel
+//
+//  Created by Dan Adajian on 2/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
