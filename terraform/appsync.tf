dec742166bebdcb620fad09118c4790f76353efa
danadajian <danadajian@gmail.com>
2022-02-05 14:36:49 -0600

    remove name index

--- terraform/appsync.tf
+++ terraform/appsync.tf
@@ -77,15 +77,9 @@ resource "aws_dynamodb_table" "bottles-table" {
   read_capacity  = 20
   write_capacity = 20
   hash_key       = "id"
-  range_key      = "name"
 
   attribute {
     name = "id"
-    type = "S"
-  }
-
-  attribute {
-    name = "name"
     type = "S"
   }
 }
