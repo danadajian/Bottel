14ec50472c275d7178dc5ebcddd714890e28da14
Dan Adajian <danadajian@gmail.com>
2022-02-05 12:27:13 -0600

    create more infra (#3)

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,13 +7,44 @@ resource "aws_cognito_user_pool" "bottel-user-pool" {
   name = "bottel-user-pool"
 }
 
-resource "aws_appsync_graphql_api" "example" {
+resource "aws_appsync_graphql_api" "bottel-api" {
   authentication_type = "AMAZON_COGNITO_USER_POOLS"
   name                = "bottel-api"
+  #  schema              = file("schema.graphql")
 
   user_pool_config {
     aws_region     = var.aws_region
     default_action = "DENY"
     user_pool_id   = aws_cognito_user_pool.bottel-user-pool.id
+  }
+}
+
+resource "aws_appsync_datasource" "example" {
+  api_id = aws_appsync_graphql_api.bottel-api.id
+  name   = "bottles-datasource"
+  type   = "AMAZON_DYNAMODB"
+
+  dynamodb_config {
+    table_name = aws_dynamodb_table.bottles-table.name
+    #    use_caller_credentials = true
+  }
+}
+
+resource "aws_dynamodb_table" "bottles-table" {
+  name           = "Bottles"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "id"
+  range_key      = "name"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "name"
+    type = "S"
   }
 }
