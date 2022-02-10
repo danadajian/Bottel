9be05c1f4cfe11048e5ca2ef18e8160aa5628d0a
danadajian <danadajian@gmail.com>
2022-02-09 23:25:26 -0600

    use sed instead

--- LastDram/CI.swift
+++
@@ -1,4 +0,0 @@
-struct CI {
-    static var apiUrl: String = "$(API_URL)"
-    static var apiKey: String = "$(API_KEY)"
-}
--- LastDram/EnvironmentVariables.swift
+++ LastDram/EnvironmentVariables.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 struct EnvironmentVariables {
-    static var apiUrl: String = ProcessInfo.processInfo.environment["API_URL"] ?? CI.apiUrl
-    static var apiKey: String = ProcessInfo.processInfo.environment["API_KEY"] ?? CI.apiKey
+    static var apiUrl: String = ProcessInfo.processInfo.environment["API_URL"] ?? "REPLACE_ME_WITH_API_URL"
+    static var apiKey: String = ProcessInfo.processInfo.environment["API_KEY"] ?? "REPLACE_ME_WITH_API_KEY"
 }
