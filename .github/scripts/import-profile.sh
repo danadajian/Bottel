e34a40fc0bef223e3b780644f3f82ae56b036549
danadajian <danadajian@gmail.com>
2022-02-09 23:36:35 -0600

    tweak

--- .github/scripts/import-profile.sh
+++ .github/scripts/import-profile.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/LastDram.mobileprovision
+echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/profile.mobileprovision
