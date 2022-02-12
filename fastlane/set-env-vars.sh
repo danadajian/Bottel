0d1a5c87491f8316bc2c9a2208631c020c99481d
danadajian <danadajian@gmail.com>
2022-02-12 10:55:46 -0600

    fix

--- fastlane/set-env-vars.sh
+++ fastlane/set-env-vars.sh
@@ -5,5 +5,5 @@ NEW_VERSION=${VERSION_TAG:1}
 echo NEW_VERSION="$NEW_VERSION"
 xcrun agvtool new-version "$NEW_VERSION"
 sed -i '' "s#MARKETING_VERSION \= [^\;]*\;#MARKETING_VERSION = $NEW_VERSION;#g" LastDram.xcodeproj/project.pbxproj
-sed -i '' "s#REPLACE_ME_WITH_API_URL#${{ secrets.API_URL }}#g" LastDram/EnvironmentVariables.swift
-sed -i '' "s#REPLACE_ME_WITH_API_KEY#${{ secrets.API_KEY }}#g" LastDram/EnvironmentVariables.swift
+sed -i '' "s#REPLACE_ME_WITH_API_URL#$API_URL#g" LastDram/EnvironmentVariables.swift
+sed -i '' "s#REPLACE_ME_WITH_API_KEY#$API_KEY#g" LastDram/EnvironmentVariables.swift
