52015a81511d5743dbd80daf76d547fc1755e1d3
danadajian <danadajian@gmail.com>
2022-02-12 01:22:42 -0600

    improve README.md

--- LastDram/Views/HomeView.swift
+++ LastDram/Views/HomeView.swift
@@ -147,10 +147,3 @@ struct ContentView_Previews: PreviewProvider {
         HomeView(userId: "123")
     }
 }
-
-extension Sequence where Iterator.Element: Hashable {
-    func unique() -> [Iterator.Element] {
-        var seen: [Iterator.Element: Bool] = [:]
-        return self.filter { seen.updateValue(true, forKey: $0) == nil }
-    }
-}
