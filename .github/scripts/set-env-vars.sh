becde86e38ff33a68c457d5d1a2f2e2382efa95d
danadajian <danadajian@gmail.com>
2022-02-12 10:48:27 -0600

    use Fastfile for build workflow

---
+++ .github/scripts/set-env-vars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VERSION_TAG=$(echo "${GITHUB_REF}" | cut -d "/" -f3)
+NEW_VERSION=${VERSION_TAG:1}
+echo NEW_VERSION="$NEW_VERSION"
+xcrun agvtool new-version "$NEW_VERSION"
+sed -i '' "s#MARKETING_VERSION \= [^\;]*\;#MARKETING_VERSION = $NEW_VERSION;#g" LastDram.xcodeproj/project.pbxproj
+sed -i '' "s#REPLACE_ME_WITH_API_URL#${{ secrets.API_URL }}#g" LastDram/EnvironmentVariables.swift
+sed -i '' "s#REPLACE_ME_WITH_API_KEY#${{ secrets.API_KEY }}#g" LastDram/EnvironmentVariables.swift
